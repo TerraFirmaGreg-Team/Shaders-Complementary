ede67bf802f9f0a076cc4c6c0f8391492feb8fa9
woopSmile <tristanjobin@gmail.com>
2026-03-28 14:22:07 -0400

    disabled sub surface scattering on snow and ice for consistency

--- shaders/lib/shaderSettings/materials.glsl
+++ shaders/lib/shaderSettings/materials.glsl
@@ -72,7 +72,7 @@
 
 #define ALTERNATIVE_AMETHYST_STYLE 0 //[0 1]
 
-#define SSS_SNOW_ICE
+//#define SSS_SNOW_ICE
 
 #define ACT_ORE_INTENSITY 1.0 //[0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
 
